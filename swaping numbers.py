bc1865acf481e2a1376060725d68287912ef1bb3
Moneesh007-tech <smoneesh.mohana@gmail.com>
2025-08-12 22:01:44 +0530

    Add files via upload

---
+++ swaping numbers.py
@@ -0,0 +1,16 @@
+#a=input('Enter the value of A=')
+#b=input('Enter the value of B=')
+#temp=a
+#a=b
+#b=temp
+#print('a= '+a)
+#print('b= '+b)
+
+
+
+#print('Moneesh')
+#print('How are you?')
+#print('Is there any problem to solve by me')
+#print('Tell me I\'ll do my level best')
+
+print(2**3)
