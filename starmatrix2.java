20d382f8715039972ca52eed9e8e8e94a67201a2
Moneesh007-tech <smoneesh.mohana@gmail.com>
2025-09-03 17:07:56 +0530

    starmatrix2.java

---
+++ starmatrix2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1; col<=n-row; col++) {
+                System.out.print(" ");
+            }
+                for(int col=1; col<=2*row-1; col++) {
+                    System.out.print("*");
+                }    System.out.println();
+
+            }
+            for(int row=n-1; row>=1; row--){
+                for(int col=1; col<=n-row;col++) {
+                    System.out.print(" ");
+                }
+                    for (int col=1; col<=2*row-1; col++) {
+                        System.out.print("*");
+                    }   System.out.println();
+
+                }
+            }
+
+        }
