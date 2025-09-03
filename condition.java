90f5f35d111a6ded1bea8c17d0e885f4f01085e7
Moneesh007-tech <smoneesh.mohana@gmail.com>
2025-09-03 17:11:14 +0530

    Create condition.java

---
+++ condition.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int a=0;
+
+        if (a==0){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
