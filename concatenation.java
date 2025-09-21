4431371fbdea50de1de5782658be4b80403d680c
Moneesh007-tech <smoneesh.mohana@gmail.com>
2025-09-21 22:32:10 +0530

    concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,8 @@
+public class ConcatenationExample {
+    public static void main(String[] args) {
+        String part1 = "Computer science";
+        String part2 = "Engineering";
+        String result = part1 + " " + part2; 
+        System.out.println(result); 
+    }
+}
